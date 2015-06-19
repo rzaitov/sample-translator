3e86991072d57d9ce480b0692bd34237655df5dc
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-06-19 23:11:43 +0300

    start to implement BindingLocator class which helps to be aware about assemblies with bounded API (e.g. Xamarin.iOS Xamarin.Mac)

---
+++ XSAddin/Translator.Core/BindingLocator/BindingLocator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Mono.Cecil;
+using System.Linq;
+
+namespace Translator.Core
+{
+	public class BindingLocator
+	{
+		readonly ModuleDefinition[] modules;
+
+		public BindingLocator (IEnumerable<string> paths)
+		{
+			modules = paths.Select (p => ModuleDefinition.ReadModule (p)).ToArray ();
+		}
+
+		public bool TryFindMethod (string className, string selector, out MethodDefinition mDef)
+		{
+			IEnumerable<TypeDefinition> types = modules.SelectMany (m => m.Types.Where (t => t.IsRegistered (className)));
+			IEnumerable<MethodDefinition> methods = types.SelectMany (t => t.Methods).Where (m => m.IsExported (selector));
+			mDef = methods.FirstOrDefault ();
+
+			return mDef != null;
+		}
+
+		bool HasRegisterAttribute(TypeDefinition typeDef, string className)
+		{
+			if (!typeDef.HasCustomAttributes)
+				return false;
+
+			return typeDef.CustomAttributes.Where (ca => ca.IsRegister (className)).Any ();
+		}
+	}
+}
---
+++ XSAddin/Translator.Core/BindingLocator/XamarinAttributesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Mono.Cecil;
+using System.Linq;
+
+namespace Translator.Core
+{
+	public static class XamarinAttributesExtensions
+	{
+		public static bool IsExport(this CustomAttribute attribute, string selector)
+		{
+			if (attribute.AttributeType.FullName != "Foundation.ExportAttribute")
+				return false;
+
+			var selectorValue = (string)attribute.ConstructorArguments [0].Value;
+			return selector == selectorValue;
+		}
+
+		public static bool IsRegister(this CustomAttribute attribute, string className)
+		{
+			if (attribute.AttributeType.FullName != "Foundation.RegisterAttribute")
+				return false;
+
+			var selectorValue = (string)attribute.ConstructorArguments [0].Value;
+			return className == selectorValue;
+		}
+
+		public static bool IsRegistered (this TypeDefinition typeDef, string className)
+		{
+			if (!typeDef.HasCustomAttributes)
+				return false;
+
+			return typeDef.CustomAttributes.Where (ca => ca.IsRegister (className)).Any ();
+		}
+
+		public static bool IsExported (this MethodDefinition mDef, string selector)
+		{
+			if (!mDef.HasCustomAttributes)
+				return false;
+
+			return mDef.CustomAttributes.Where (ca => ca.IsExport (selector)).Any ();
+		}
+	}
+}
