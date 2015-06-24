2d174f0d52583dd3f06f38d0df632121d607c7ab
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-06-24 20:34:27 +0300

    make integration

--- XSAddin/ProjectTranslator/Utilities/CodeTranslator.cs
+++ XSAddin/ProjectTranslator/Utilities/CodeTranslator.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Text;
 using System.Reflection;
+using Translator.Core;
 
 namespace ProjectTranslator
 {
@@ -42,17 +43,48 @@ namespace ProjectTranslator
 
 			var sb = new StringBuilder ();
 			// sample-translator tool's parameters
-			sb.AppendFormat (" -ns {0}", configuration.ProjectNamespace);
-			sb.AppendFormat (" -dir \"{0}\"", configuration.ProjectPath);
-			sb.AppendFormat (" {0}", configuration.FilesToString ()); // source files
-			sb.AppendFormat (" --"); // clang parameters begins here
-			sb.Append (" -ObjC");
-			sb.AppendFormat (" -F\"{0}\"", XCodeConfiguration.PathToFramewroks);
-			sb.Append (" -mios-simulator-version-min=8.4");
+//			sb.AppendFormat (" -ns {0}", configuration.ProjectNamespace);
+//			sb.AppendFormat (" -dir \"{0}\"", configuration.ProjectPath);
+//			sb.AppendFormat (" {0}", configuration.FilesToString ()); // source files
+//			sb.AppendFormat (" --"); // clang parameters begins here
+//			sb.Append (" -ObjC");
+//			sb.AppendFormat (" -F\"{0}\"", XCodeConfiguration.PathToFramewroks);
+//			sb.Append (" -mios-simulator-version-min=8.4");
 			sb.AppendFormat (" {0}", configuration.FramewroksToString ());
 			sb.Append (" -fmodules -fobjc-arc");
 			sb.AppendFormat (" -isysroot {0}", XCodeConfiguration.SdkPath);
 			sb.AppendFormat (" -resource-dir \"{0}\"", resources);
+
+			List<string> clangArgs = new List<string> {
+				"-ObjC",
+				string.Format ("-F\"{0}\"", XCodeConfiguration.PathToFramewroks),
+				"-mios-simulator-version-min=8.4",
+				"-fmodules",  // enable modules
+				"-fobjc-arc", // enable ARC
+				"-isysroot", XCodeConfiguration.SdkPath,
+				"-resource-dir", resources
+			};
+			foreach (var f in configuration.Frameworks) {
+				clangArgs.Add ("-framework");
+				clangArgs.Add (f);
+			}
+
+			var pathLocator = new XamarinPathLocator ();
+			string xi = pathLocator.GetAssemblyPath (Platform.iOS);
+			var locator = new BindingLocator (new string[] { xi });
+
+			SourceCodeTranslator srcTranslator = new SourceCodeTranslator (clangArgs.ToArray (), locator);
+
+			foreach (var file in configuration.FilePaths) {
+				Console.WriteLine (file);
+				string fileName = Path.GetFileNameWithoutExtension (file);
+				fileName = Path.ChangeExtension (fileName, "cs");
+				var dstPath = Path.Combine (configuration.ProjectPath, fileName);
+
+				using (var textWriter = File.CreateText (dstPath)) {
+					srcTranslator.Translate (file, configuration.ProjectNamespace, textWriter);
+				}
+			}
 
 			info.FileName = translatorPath;
 			info.WorkingDirectory = utilsPath;
