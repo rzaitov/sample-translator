c22f4d3a1e03c2161bedef3a55a9a15348fd3c4f
olegoid <olegoid91@gmail.com>
2015-07-02 19:14:50 +0300

    Change adding name, menu label, UI window title

--- XSAddin/Translator.Addin/Properties/AddinInfo.cs
+++ XSAddin/Translator.Addin/Properties/AddinInfo.cs
@@ -3,14 +3,14 @@ using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly:Addin (
-	"Project Convertor", 
+	"ProjectTranslator",
 	Namespace = "Translator.Addin",
 	Category = "Project Import and Export",
 	Url = "http://xamarin.com/",
 	Version = "1.0"
 )]
 
-[assembly:AddinName ("Project Convertor")]
+[assembly:AddinName ("ProjectTranslator")]
 [assembly:AddinCategory ("Project Import and Export")]
 [assembly:AddinDescription ("Convert Xcode project to Xamarin.iOS project")]
 [assembly:AddinAuthor ("Oleg Demchenko & Rustam Zaitov")]
