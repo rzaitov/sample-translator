222f5fc7c2209db5444fa45a66de18e4fe4e1382
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-07-27 20:16:45 +0300

    add helper class to store code on disk

---
+++ XSAddin/Translator.Test/ClangManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Translator.Test
+{
+	public class ClangManager : IDisposable
+	{
+		string tmpDir;
+		public string TmpFolder {
+			get {
+				if (tmpDir == null) {
+					tmpDir = Path.Combine (Path.GetTempPath (), Path.GetRandomFileName ());
+					Directory.CreateDirectory (tmpDir);
+				}
+
+				return tmpDir;
+			}
+		}
+
+		public ClangManager ()
+		{
+		}
+
+		public string StoreFile (string content, string fileName)
+		{
+			string filePath = Path.Combine (TmpFolder, fileName);
+			File.WriteAllText (filePath, content);
+
+			return filePath;
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose ()
+		{
+			if (tmpDir != null)
+				Directory.Delete (tmpDir);
+		}
+
+		#endregion
+	}
+}
+
---
+++ XSAddin/Translator.Test/ClangManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Translator.Test
+{
+	[TestFixture]
+	public class ClangManagerTest
+	{
+		[Test]
+		public void StoreContent ()
+		{
+			var manager = new ClangManager ();
+			const string content = "Hello!";
+			string filePath = manager.StoreFile (content, "tmp.m");
+
+			Assert.AreEqual (content, File.ReadAllText (filePath));
+		}
+	}
+}
+
