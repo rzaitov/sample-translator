793134d8c635a597709f9a2a91955e2a2525e829
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-08-14 19:36:26 +0300

    add index helper class

---
+++ XSAddin/Translator.Core/Helpers/Index.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ClangSharp;
+
+namespace Translator.Core
+{
+	public class Index
+	{
+		readonly HashSet<string> index = new HashSet<string> ();
+
+		public void Put (CXCursor cursor)
+		{
+			index.Add (clang.getCursorUSR (cursor).ToString ());
+		}
+
+		public bool Contains (CXCursor cursor)
+		{
+			return index.Contains (clang.getCursorUSR (cursor).ToString ());
+		}
+	}
+}
